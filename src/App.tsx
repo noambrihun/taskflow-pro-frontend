ea90846f0ad7da53d4799bda4976f581e895ff83
noambrihun <noambrihun1@gmail.com>
2026-04-27 21:26:19 +0300

    first commit for this taskflow project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>Hello World</h1>
+      <p className='text-lg'>This is a paragraph</p>
+      <button className='bg-blue-500 text-white p-2 rounded-md'>Click me</button>
+    </>
+  )
+}
+
+export default App
