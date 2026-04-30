0eb58f4158e30497be913fb3b9cda0a0634511a6
noambrihun <noambrihun1@gmail.com>
2026-04-30 22:54:38 +0300

    add status and priority fields to UI

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -3,6 +3,8 @@ type Task = {
     title: string
     description: string
     completed: boolean
+    status: string
+    priority: string
   }
   
   type Props = {
@@ -13,11 +15,22 @@ type Task = {
     return (
       <div className="bg-white shadow-md rounded-lg p-4 flex justify-between items-center">
         
-        <div>
+         <div>
           <h2 className="text-lg font-bold">{task.title}</h2>
           <p className="text-gray-600">{task.description}</p>
+         </div>
+         <div>
+           <p className={`text-sm font-semibold ${
+            task.priority === "High"
+            ? "text-red-500"
+            : task.priority === "Medium"
+            ? "text-yellow-500"
+            : "text-green-500"
+     }`}>
+      Priority: <span className="font-bold">{task.priority}</span>
+          </p>
         </div>
-  
+
         <div className="flex gap-2">
           <button className="bg-green-500 text-white w-24 px-3 py-2 rounded-md">
             {task.completed ? "Done" : "Mark as Done"}
